eb5517fee5f18e62769cf6cb265239a2fb6ef112
Chen Peng <chenpeng.net@gmail.com>
2020-02-12 19:06:31 +0800

    完善login页

--- lib/home/login.dart
+++ lib/home/login.dart
@@ -1,13 +1,11 @@
-import 'dart:io';
-import 'dart:typed_data';
-
+import 'package:acai_flutter/home/home.dart';
 import 'package:acai_flutter/util/DioUtils.dart';
 import 'package:encrypt/encrypt.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'dart:convert';
 
 import 'package:oktoast/oktoast.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Login extends StatefulWidget {
   Login({this.text});
@@ -20,6 +18,7 @@ class Login extends StatefulWidget {
 
 class LoginState extends State<Login> {
   final TextEditingController usernameController = new TextEditingController();
+  final TextEditingController nicknameController = new TextEditingController();
   final TextEditingController passwordController = new TextEditingController();
   var publicKey;
 
@@ -32,62 +31,38 @@ class LoginState extends State<Login> {
   getPublicKey() async {
     var url = '/api/oauth/publicKey';
     var dio = await DioUtils.getDio();
-    try {
-      var response = await dio.get(url);
-      if (response.statusCode == HttpStatus.ok) {
-        var data = response.data;
-        var code = data['Code'];
-        if (code == "0") {
-          setState(() {
-            var str = data['Data'];
-            final parser = RSAKeyParser();
-            publicKey = parser.parse(str);
-            print("公钥：");
-            print(str);
-          });
-        } else {
-          showToast("获取公钥失败");
-        }
-      } else {
-        print('查询失败:${response.statusCode}');
-      }
-    } catch (e) {
-      print('查询失败');
-    }
+    var response = await dio.get(url);
+    var data = response.data;
+    setState(() {
+      var str = data['data'];
+      final parser = RSAKeyParser();
+      publicKey = parser.parse(str);
+    });
   }
 
   Future signUp() async {
     String username = usernameController.text;
+    String nickname = nicknameController.text;
     String password = passwordController.text;
-    if (username == '' || password == '') {
+    if (username == '' || nickname == '' || password == '') {
       return;
     }
-    String text = username + ";" + password;
+    String text = username + ";" + nickname + ";" + password;
     var iv = IV.fromSecureRandom(256);
     var random = iv.base64;
     String encrypted =
         Encrypter(RSA(publicKey: publicKey)).encrypt(text, iv: iv).base64;
-    print(encrypted);
     var url = '/api/oauth/signUp';
     var dio = await DioUtils.getDio();
-    try {
-      var response =
-          await dio.post(url, data: {'Text': encrypted, 'Random': random});
-      if (response.statusCode == HttpStatus.ok) {
-        var data = response.data;
-        var code = data['Code'];
-        if (code == "0") {
-          print("返回时:");
-          print(data['Data']);
-          showToast("注册成功");
-        } else {
-          showToast("注册失败");
-        }
-      } else {
-        print('查询失败:${response.statusCode}');
-      }
-    } catch (e) {
-      print('查询失败');
+    var response =
+        await dio.post(url, data: {'text': encrypted, 'random': random});
+    var data = response.data;
+    var code = data['code'];
+    if (code == 0) {
+      showToast("注册成功");
+    } else {
+      var message = data['message'];
+      showToast(message);
     }
   }
 
@@ -105,30 +80,32 @@ class LoginState extends State<Login> {
     print(encrypted);
     var url = '/api/oauth/signIn';
     var dio = await DioUtils.getDio();
-    try {
-      var response =
-          await dio.post(url, data: {'Text': encrypted, 'Random': random});
-      if (response.statusCode == HttpStatus.ok) {
-        var data = response.data;
-        var code = data['Code'];
-        if (code != '0') {
-          showToast("登录失败");
-        }
-        print("返回时:");
-        print(data['Data']);
-      } else {
-        print('查询失败:${response.statusCode}');
-      }
-    } catch (e) {
-      print('查询失败');
+    var response =
+        await dio.post(url, data: {'text': encrypted, 'random': random});
+    var data = response.data;
+    var code = data['code'];
+    if (code == 0) {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      prefs.setString("accessToken", data['data']);
+      Navigator.push(
+        context,
+        new MaterialPageRoute(
+          builder: (context) => new MyHomePage(title: '阿财'),
+        ),
+      );
+    } else {
+      var message = data['message'];
+      showToast(message);
     }
   }
 
   @override
   Widget build(BuildContext context) {
+    DioUtils.ctx = context;
     return Scaffold(
       appBar: AppBar(
-        title: Text("widget.title"),
+        title: Text("登录注册"),
+        automaticallyImplyLeading: false,
       ),
       body: Column(
         children: <Widget>[
@@ -140,6 +117,18 @@ class LoginState extends State<Login> {
                     decoration: InputDecoration(
                       border: OutlineInputBorder(),
                       labelText: '账号',
+                    )),
+              ),
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: TextField(
+                    controller: nicknameController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: '昵称',
                     )),
               ),
             ],
