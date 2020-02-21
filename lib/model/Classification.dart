895c3bd57b6c6625ecc59edcd133dd78f6e25ebe
Chen Peng <chenpeng.net@gmail.com>
2020-02-21 16:21:57 +0800

    增加实体类

--- lib/model/Classification.dart
+++ lib/model/Classification.dart
@@ -2,5 +2,12 @@ class Classification {
   int code;
   String name;
 
-  Classification(this.code, this.name);
+  Classification({this.code, this.name});
+
+  factory Classification.fromJson(Map map) {
+    return new Classification(
+      code: map['code'],
+      name: map['name'],
+    );
+  }
 }
