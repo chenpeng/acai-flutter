30de5232b672437052f72f54bbbe72fec3d93076
Chen Peng <chenpeng.net@gmail.com>
2020-04-26 18:10:26 +0800

    加上web版

---
+++ android/app/src/main/kotlin/com/example/acai_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.acai_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/io/chenpeng/acai_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package io.chenpeng.acai_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
